f02e8d9b14f8262fdc3cec60049e245650d84962
Ernest <ernest@clickverta.com>
2020-02-02 12:26:55 +0200

    Init

---
+++ SP-patkenny.php
@@ -0,0 +1,19 @@
+<h1>Pat Kenny is relenting in the fight to stop construction next to Dalkey’s home</h1>
+<div class="feature-box">
+    <!-- Featured image same as Ad -->
+    <img src="" alt="Title">
+</div>
+<h2>Broadcaster asks for consideration on a ‘ costly ‘ legal route to avoid apartment building</h2>
+<p>Broadcaster Pat Kenny gave up his fight to stop the construction of apartment blocks on lands next to his home in Dalkey.</p>
+<p>On Friday, though, the Newstalk host said he was “completely behind” a High Court attempt to prevent the same developer at Bulloch Harbour in another area of Dalkey from building a mixed-use site. In the case of the An Bord Pleanála Bulloch Harbor ruling, people have already collected € 30,000 to finance the High Court judicial review lawsuit.</p>
+<p>The planning board overturned a decision by Dún Laoghaire Rathdown County Council in a decision last July to grant Richard Barrett’s Bartra permission for apartments on lands nearby to the Kenny home.</p>
+<p>Despite strong resistance from Mr. Kenny and his wife Kathy, along with other local residents, the board of appeals accepted the proposal. The Kennys and other objectors had the right in the courts to contest the ruling of the appeals board.</p>
+<p>Yet today Mr. Kenny said: “The deadline has passed for judicial review. Judicial review is extremely expensive and even if you win there is no guarantee that you will be awarded your legal costs.</p>
+<p>“And if you win, there is nothing to stop a developer going in again and tweaking things and forcing you through the whole process all over again with no certainty of success . . . All we can do now is insist that the council enforce the strict conditions laid down by An Bord Pleanála.”</p>
+<p>The planning board reversed inspector findings in both Bartra cases and granted permission.</p>
+<p>But, in a bid to stop the other Bartra development process at Bulloch Harbour, locals are going down the judicial review path.</p>
+<p>Last week, at the Killiney Castle Hotel, the Bulloch Harbor Heritage Association organized a public meeting attended by about 250 residents.</p>
+<p>District TD Richard Boyd Barrett and State Minister for Education Mary Mitchell O’Connor were also present along with Pat and Kathy Kenny.</p>
+<p>Mr. Kenny recently opposed the judgment of An Bord Pleanála, defining it as“wrong-headed and has to be stopped”.</p>
+<p>The heritage group obtained leave at the High Court in early October to appeal the ruling of the planning board and the argument was delayed to December.</p>
+
